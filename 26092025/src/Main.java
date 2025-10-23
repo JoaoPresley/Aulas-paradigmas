0bfa7ccf07a510243326d8c2cd130f59b9c05040
João Alencar <jvalencarmg@gmail.com>
2025-10-23 18:40:08 -0400

    criada atividade do dia 26092025

---
+++ 26092025/src/Main.java
@@ -0,0 +1,15 @@
+/*
+Para esta atividade, vocês devem utilizar o que já vimos até agora.
+
+Utilizando uma proposta própria de solução ou o conceito de "Grandes Ícones Históricos (Pessoas)" você deve criar seu projeto.
+
+O projeto deve conter, pelo menos:
+3 vetores (1 para Bubble, 1 para Insertion e 1 para Selection);
+3 Ordenações vistas em cada vetor;
+e conter a alteração do vetor.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
